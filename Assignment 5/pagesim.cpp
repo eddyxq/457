9405b1601edc2d64a4e9b0f436b8696d4167515f
eddyxq <eddy@ualberta.ca>
2019-04-15 19:52:31 -0600

    457

--- Assignment 5/pagesim.cpp
+++ Assignment 5/pagesim.cpp
@@ -8,14 +8,7 @@
 
 using namespace std;
 
-/**
- * Assignment 5
- * CPSC 457 - L01 - T05
- * Eddy Qiang - 30058191
- */
- 
-vector<int> v;
-
+//method returns true if given integer is exists inside array
 int inArray(int n, int a[], int size)
 {
     for (int i = 0; i<size; i++)
@@ -28,6 +21,7 @@ int inArray(int n, int a[], int size)
     return 0;
 }
 
+//optimal page replacement algorithm
 void optimal(int numFrames, vector<int> v)
 {
     unordered_map<int, int> mymap;
@@ -62,17 +56,21 @@ void optimal(int numFrames, vector<int> v)
 				count -= 1;
 			}
         }
-		//when frame is full
+		else if (inArray(v[i], frame, numFrames))
+		{
+			//do nothing when it is a hit
+		}
+		//else determine where to swap 
         else
         {
 			//look forward
             if (!inArray(v[i], frame, numFrames))
             {
                 //for each of the numbers in the frame, find the index of the next occruence
-                for (int i = 0; i<numFrames; i++)
+                for (int i = 0; i < numFrames; i++)
                 {
                     int isInFuture = 0;
-                    for (int j = currentIndex; j<v.size(); j++)
+					for (int j = v.size()-1; j > currentIndex; j--)
                     {
                         if (frame[i] == v[j])
                         {
@@ -88,7 +86,7 @@ void optimal(int numFrames, vector<int> v)
                 int indexThatHasMax;
                 int max = 0;
                 //find the highest index found, which is the one we should replace
-                for (int i = 0; i<numFrames; i++)
+                for (int i = 0; i < numFrames; i++)
                 {
                     int temp = mymap[frame[i]];
 
@@ -112,12 +110,13 @@ void optimal(int numFrames, vector<int> v)
     {
         frameOutput += to_string(frame[i]) + " ";
     }
-
+	//display output
     cout << "Optimal:" << endl;
-    cout << "  - frames: " << frameOutput << endl;
-    cout << "  - page faults: " << numPageFaults << endl;
+    cout << " - frames: " << frameOutput << endl;
+    cout << " - page faults: " << numPageFaults << endl;
 }
 
+//least recently used page replacement algorithm
 void lru(int numFrames, vector<int> v)
 {
     unordered_map<int, int> mymap;
@@ -151,6 +150,11 @@ void lru(int numFrames, vector<int> v)
 				count -= 1;
 			}
         }
+		else if (inArray(v[i], frame, numFrames))
+		{
+			//do nothing when it is a hit
+		}
+		//else determine where to swap 
         else
         {
 			//look backward
@@ -195,158 +199,103 @@ void lru(int numFrames, vector<int> v)
     {
         frameOutput += to_string(frame[i]) + " ";
     }
-
+	//display output
     cout << "LRU:" << endl;
-    cout << "  - frames: " << frameOutput << endl;
-    cout << "  - page faults: " << numPageFaults << endl;
+    cout << " - frames: " << frameOutput << endl;
+    cout << " - page faults: " << numPageFaults << endl;
 }
 
-
+//method that increments the pointer to wrap around like a clock head
 int incrementClockPointer(int current, int max)
 {
-    if (current < max)
-    {
-        return current + 1;
-    }
-    return 0;
+    return (current + 1) % max;
 }
 
+//clock replacement (aka second chance) algorithm
 void clock(int numFrames, vector<int> v)
 {
-    int numPageFaults = 0;
-	int count = 0;
+	//array containing the frame numbers
     int frame[numFrames];
+	//array that tracks the clock bit
     int flag[numFrames];
+	//index of the clock pointer
     int pointer = 0;
+	//variable tracking the number of page faults
+	int numPageFaults = 0;
 
-    //initialize all array values to -1
-    for (int i = 0; i<numFrames; i++)
+    //initialize array values
+    for (int i = 0; i < numFrames; i++)
     {
+		//-1 means frame location is empty
         frame[i] = -1;
+		//initial bit is 0
         flag[i] = 0;
     }
 
     //for each number in vector
-    for (int i = 0; i<v.size(); i++)
+    for (int i = 0; i < v.size(); i++)
     {
-        // cout << "index is pointing to: " << pointer << endl;
-
-
-
-
-        //if can find -1, not full
+        //if array location is empty (marked by -1)
         if (inArray(-1, frame, numFrames))
         {
             //if it's not already there
             if (!inArray(v[i], frame, numFrames))
             {
-                //add to frame at index count
-                frame[count] = v[i];
-                flag[count] = 1;
+                //add to frame, increment page fault, and set bit to 1
+                frame[pointer] = v[i];
+                flag[pointer] = 1;
                 numPageFaults += 1;
-
-                if (pointer < numFrames-1)
-                {
-                    pointer++;
-                }
-                else
-                {
-                    pointer = 0;
-                }
-
-                
+				//move pointer to next location
+                pointer = incrementClockPointer(pointer, numFrames);
             }
         }
-        else
-        {
-            if (!inArray(v[i], frame, numFrames))
-            {
-
-                int notReplaced = 1;
-                
-                while(notReplaced)
-                {
-                    //look if flag is 0
-                    //for each frame
-                    for (int i = 0; i<numFrames; i++)
-                    {   
-                        // cout << "flag pointer value " << flag[pointer] << endl;
-
-
-                        //if 0, replace
-                        if (flag[pointer] == 0)
-                        {
-                            // cout << "replaced " << frame[pointer] << " with " << v[i] << endl;
-
-                            frame[pointer] = v[i];
-                            numPageFaults += 1;
-                            notReplaced = 0;
-                            flag[pointer] = 1; 
-                        }
-                        //if 1, decrease to 0
-                        else if (flag[pointer] == 1)
-                        {
-                            flag[pointer] = 0;
-                        }
-
-
-                        //move the pointer
-                        // cout << "index: " << pointer << endl;
-                        //pointer = incrementClockPointer(pointer, numFrames-1);
-                        //pointer ++;
-                        if (pointer < numFrames-1)
-                        {
-                            pointer++;
-                        }
-                        else
-                        {
-                            pointer = 0;
-                        }
-
-
-                        if (notReplaced = 0)
-                        {
-                            break;
-                        }
-                    }
-                }
-
-
-                
-
-
-            }
-        }
-
-
-        cout << "current state after seeing " << v[i] << endl;
-
-        
-        for (int i = 0; i<numFrames; i++)
-        {
-            cout << frame[i] << endl;
-        }
-
-
-        count += 1;
+		else if (inArray(v[i], frame, numFrames))
+		{
+			//do nothing when it is a hit
+		}
+		//else determine where to swap 
+		else
+		{
+			while(true)
+			{
+				//find the next frame with bit counter 0
+				if (flag[pointer] == 1)
+				{
+					//if it's 1, decrement and move to next
+					flag[pointer] = 0;
+					pointer = incrementClockPointer(pointer, numFrames);
+				}
+				//if it's 0, replace this frame and increment pagefault
+				else
+				{
+					//add to frame, increment page fault, and set bit to 1
+					frame[pointer] = v[i];
+					flag[pointer] = 1;
+					numPageFaults += 1;
+					//move pointer to next location
+					pointer = incrementClockPointer(pointer, numFrames);
+					break;
+				}
+			}
+		}
     }
 
-
-    string frameOutput;// = "4 5 2 3";
-    //numPageFaults = 10;
+    string frameOutput;
     //get the state of frames at the end
     for (int i = 0; i<numFrames; i++)
     {
         frameOutput += to_string(frame[i]) + " ";
     }
-
+	//display output
     cout << "Clock:" << endl;
-    cout << "  - frames: " << frameOutput << endl;
-    cout << "  - page faults: " << numPageFaults << endl;
+    cout << " - frames: " << frameOutput << endl;
+    cout << " - page faults: " << numPageFaults << endl;
 }
 
 int main(int argc, char *argv[])
 {
+	//vector that holds each of the numbers read in from text file
+	vector<int> v;
     int numFrames = 0;
 
     if(argc != 2) 
@@ -371,9 +320,10 @@ int main(int argc, char *argv[])
             v.push_back(stoi(t));
         }
 	}    
-
-    //optimal(numFrames, v);
-    //lru(numFrames, v);
+	
+	//run each algorithm
+    optimal(numFrames, v);
+    lru(numFrames, v);
     clock(numFrames, v);
 
 	return 0;
