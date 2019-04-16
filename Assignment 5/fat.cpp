9b379a1ea88b5f20da89fd5cfe81ef5a9a1bad8e
eddyxq <eddy@ualberta.ca>
2019-04-15 20:17:54 -0600

    457

--- Assignment 5/fat.cpp
+++ Assignment 5/fat.cpp
@@ -1,15 +1,3 @@
-// CPSC457 University of Calgary
-// Skeleton C++ program for Q7 of Assignment 5.
-//
-// The program reads in the input, then calls the (wrongly implemented) checkConsistency()
-// function, and finally formats the output.
-//
-// You only need to reimplement the checkConsistency() function.
-//
-// Author: Pavol Federl (pfederl@ucalgary.ca or federl@gmail.com)
-// Date: April 1, 2019
-// Version: 5
-
 #include <stdio.h>
 #include <string>
 #include <vector>
@@ -23,7 +11,8 @@ using namespace std;
 typedef std::string SS;
 typedef std::vector<SS> VS;
 
-struct DEntry {
+struct DEntry 
+{
     SS fname = SS( 4096, 0);
     int size = 0;
     int ind = 0;
@@ -31,18 +20,16 @@ struct DEntry {
     bool tooFewBlocks = false;
     bool hasCycle = true;
     bool sharesBlocks = true;
-
     set<int> traversedblocks;
-
 };
 
-static SS join( const VS & toks, const SS & sep) {
+static SS join( const VS & toks, const SS & sep) 
+{
     SS res;
     bool first = true;
     for( auto & t : toks) { res += (first ? "" : sep) + t; first = false;}
     return res;
 }
-
 
 int blocksIntersect(set<int> s1, set<int> s2)
 {
@@ -52,44 +39,36 @@ int blocksIntersect(set<int> s1, set<int> s2)
     return intersect.size() > 0 ? 1 : 0;
 }
 
-// re-implement this function
-//
-// Parameters:
-//   blockSize - contains block size as read in from input
-//   files - array containing the entries as read in from input
-//   fat - array representing the FAT as read in from input
-// Return value:
-//   the function should return the number of free blocks
-//   also, for ever entry in the files[] array, you need to set the appropriate flags:
-//      i.e. tooManyBlocks, tooFewBlocks, hasCycle and sharesBlocks
 int checkConsistency( int blockSize, std::vector<DEntry> &files, std::vector<int> &fat)
 {
+	//keeps track of the blocks used from all the files
     set<int> blockUsed;
-
     //for each file
     for (int i = 0; i < files.size(); i++)
-    //for (int i = 6; i < 7; i++)
     {
-		
-		cout << "file size: " << files[i].size << endl;
-		
         //set everything to false to start
         files[i].hasCycle = false;
         files[i].tooFewBlocks = false;
         files[i].tooManyBlocks = false;
         files[i].sharesBlocks = false;
 
-
         //calc number of blocks
         int numBlocks = ceil(float(files[i].size) / float(blockSize));
 
         //calc how many blocks used in table
         int current = files[i].ind;
-        set<int> set; 
 		
-		int setSize = 0;
+		//initialize array to -1, used to keep track if a frame has been used
+		int arr [200000];
+		for (int i = 0; i <= 200000; i++) 
+		{
+			arr[i] = -1;
+		}
+		
+		//count the number of blocks a file uses
+		int blockCount = 0;
 
-
+		//flag
         int notEmpty = 1;
         //check if file is empty
         if(files[i].ind == -1)
@@ -104,32 +83,29 @@ int checkConsistency( int blockSize, std::vector<DEntry> &files, std::vector<int
                 //not enough blocks
                 files[i].tooFewBlocks = true;
             }
-
         }
-
+		//when the file is not empty
         while(notEmpty)
         {
-
-            //if it points to -1 break
+            //if it points to -1 break because you are at end of file
             if (fat[current] == -1)
             {
-                set.insert(current);
-				setSize++;
+				//increment count and mark block as used
+				blockCount++;
+				arr[current] = 1;
                 files[i].traversedblocks.insert(current);
                 blockUsed.insert(current);
-
-                //cout << "BROKEN OUT OF LOOP HERE" << endl;
                 break;
             }
             else 
             {
                 //check if current index is in the set
-                if (find(set.begin(), set.end(), fat[current]) != set.end() || current == fat[current])
+                if (arr[fat[current]] == 1|| current == fat[current])
                 {
                     //if already in set, means there is cycle, so break
                     files[i].hasCycle = true;
-                    set.insert(current);
-					setSize++;
+					blockCount++;
+					arr[current] = 1;
                     files[i].traversedblocks.insert(current);
                     blockUsed.insert(current);
                     break;
@@ -138,32 +114,18 @@ int checkConsistency( int blockSize, std::vector<DEntry> &files, std::vector<int
                 else
                 {
                     files[i].traversedblocks.insert(current);
-                    set.insert(current);
-					setSize++;
+					blockCount++;
+					arr[current] = 1;
                     blockUsed.insert(current);
                 }
                 //change pointer
                 current = fat[current];
-
-
             }
         }
 
-
-        //cout <<" this set has :" << endl;
-        //for (auto it=set.begin(); it != set.end(); ++it) 
-        //cout << ' ' << *it; 
-		
-		cout << "size of set: " << set.size() << endl;
-		cout << "new set num: " << setSize << endl;
-
         //check if too many blocks or too few blocks
-        int min = (set.size()-1) * blockSize + 1;
-        int max = (set.size() * blockSize);
-
-        cout << "min " << min << endl;
-        cout << "max " << max << endl;
-
+        int min = (blockCount-1) * blockSize + 1;
+        int max = (blockCount * blockSize);
 
         if (files[i].ind != -1 && files[i].size < min)
         {
@@ -175,11 +137,10 @@ int checkConsistency( int blockSize, std::vector<DEntry> &files, std::vector<int
         }
     }
 
-    //now check if any of the sets intersect
     //for each file
     for (int i = 0; i < files.size(); i++)
     {
-        //if any of the files intersect with another file break
+        //checking if any of the sets of blocks intersect
         for (int j = 0; j < files.size(); j++)
         {
             if ((i != j) && blocksIntersect(files[i].traversedblocks, files[j].traversedblocks))
@@ -190,9 +151,8 @@ int checkConsistency( int blockSize, std::vector<DEntry> &files, std::vector<int
         }
     }
 
-    // finally, return the number of free blocks
+    // finally return the number of free blocks
     return fat.size() - blockUsed.size();
-    //return 0;
 }
 
 int main()
